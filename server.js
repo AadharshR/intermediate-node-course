8fc1c1c3b3299106094dc025affd9282f5af1e52
AadharshR <aadharshr6@gmail.com>
2021-08-24 14:55:58 +0530

    add get by id and mongo changes

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express= require('express');
 const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
 const User=require('./models/User');
-mongoose.connect('mongodb://localhost/userData')
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.connect('mongodb://localhost/userData',{ useNewUrlParser: true,useUnifiedTopology: true} )
 
 const port=8000;
 const app= express();
@@ -16,11 +18,51 @@ app.listen(port, ()=>{
 // CREATE
 app.post('/users',(req,res)=>{
   // User.create()
+  console.log("req",req.body)
+  User.create(
+    {
+      name: req.body.newData.name,
+      email: req.body.newData.email,
+      password: req.body.newData.password
+    },
+    (err,data) => {
+      if (err) {
+        console.log("Error is ", err);
+        res.json({success:false,message:err})
+      }
+      else if (!data) {
+        console.log("data is", data);
+        res.json({success:false,message:"Not found"})
+
+      }
+      else 
+        console.log("Data not present");
+        res.json({success:true,data:data})
+    }
+  )
 })
 
 app.route('/users/:id')
 // READ
 .get((req,res)=>{
+  User.findById(req.params.id,(err,data) => {
+    if (err) {
+      res.json({success:false,message:"An error occured"})
+    }
+    else if (!data) {
+       res.json (
+       {
+        success:false,
+        message:"Not Found"
+       })
+   }
+   else {
+     res.json ({
+       success:true,
+       data: data
+     })
+   }
+  })
   // User.findById()
 })
 // UPDATE
